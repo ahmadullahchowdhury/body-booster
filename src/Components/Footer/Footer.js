84021aad3d0e447f06d9341d61af8471fe992776
kowchergit config --global user.email kowcher99@gmail.com <37480985+kowcher99@users.noreply.github.com>
2022-09-29 13:11:31 +0600

    footed added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './Footer.css'
 const Footer = () => {
     return (
         <div>
-            <p>Footer</p>
+            <p className='footer'>Copyright @ A H M Ahmadullah Chowdhury 2022</p>
         </div>
     );
 };
